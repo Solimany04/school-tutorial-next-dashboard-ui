7cee5d7469354b11c5099474071d88cb97340da9
Solimany04 <aabkrino12@gmail.com>
2025-12-13 20:26:20 +0200

    home page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''><p>Home</p>
+    <Link href={`/admin`} className="p-4 bg-AbduhSky m-2">go to admin</Link>
+    </div>
   )
 }
 
