c35adaa40e95e4e99d4ba0025c678b6633990092
Solimany04 <aabkrino12@gmail.com>
2025-12-16 10:12:12 +0200

    classes + subjects list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { ChartRadialSimple } from "@/components/shadcnRadialChart"
 import Link from "next/link"
 
 const Homepage = () => {
   return (
     <div className=''><p>Home</p>
     <Link href={`/admin`} className="p-4 bg-AbduhSky m-2">go to admin</Link>
+    <div className="m-10">
+      <ChartRadialSimple/>
     </div>
+    </div>
+
   )
 }
 
